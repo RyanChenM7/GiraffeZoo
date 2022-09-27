ceed5e9249da132015ff8cc90fe3990b6736c531
Martin Ye <martinye022@gmail.com>
2022-09-27 18:13:52 -0400

    index

---
+++ my-app/src/routes/index.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    let count = 0;
+    function clicked() {
+        count++;
+    }
+</script>
+
+<style>
+
+</style>
+
+<!--HTML BELOW-->
+<button on:click={clicked}>
+    Clicked {count} times
+</button>
