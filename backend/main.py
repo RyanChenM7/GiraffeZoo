3fd63e8763635e2baed68f130f25efc31f574e6d
Martin Ye <martinye022@gmail.com>
2022-09-27 21:36:01 -0400

    WE ARE DONNEEEEEE SEEEESH

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,35 @@
+from re import M
 from flask import Flask
 from flask_cors import CORS
+from flaskext.mysql import MySQL
 
 app = Flask(__name__)
 CORS(app)
+mysql = MySQL()
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'Noodl3s!'
+app.config['MYSQL_DATABASE_DB'] = 'testDB'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
+conn = mysql.connect()
+cursor =conn.cursor()
+
+cursor.execute("SELECT * from student")
+data = cursor.fetchone()
+print("data", data)
 
 @app.route("/")
 def hello_word():
     data = { "content": "Hello World", "message": "success"}
+    return data, 200
+
+@app.route("/testsql")
+def get_Data():
+    conn = mysql.connect()
+    cursor =conn.cursor()
+
+    cursor.execute("SELECT * from student")
+    data = cursor.fetchone()
+    print("data", data)
+    data = { "content": data, "message": "success"}
     return data, 200
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -6,7 +6,7 @@
         data_loading = true
         count++;
         
-        const response = await fetch("http://localhost:5000/",
+        const response = await fetch("http://localhost:5000/testsql",
             {mode: 'cors',
             headers: {
                 'Access-Control-Allow-Origin':'*'
