98c4c7c5f6583139772a4bda13e7bf2d18387f60
Martin Ye <martinye022@gmail.com>
2022-10-19 20:53:16 -0400

    basic frontend stuff

--- frontend/src/components/Header.svelte
+++ frontend/src/components/Header.svelte
@@ -4,8 +4,15 @@
 	 */
     export let headerStuff = "test";
 </script>
+
+<style>
+.main-header {
+    background-color: green;
+}
+</style>
 <section>
-    <div>
-        <h1>THIS IS A TEST HEADER put stuff here: {headerStuff}</h1>
+    <div class="row main-header">
+        <div class="col-11"><h1>{headerStuff}</h1></div>
+        <div class="col"><button href='/test'>Make your Posting</button></div>
     </div>
 </section>
