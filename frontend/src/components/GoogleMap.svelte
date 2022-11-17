92ffc88493e22bd1105d4798ee6f739aa0187f40
Martin Ye <martinye022@gmail.com>
2022-11-17 12:04:14 -0500

    google maps

---
+++ frontend/src/components/GoogleMap.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+    import type {ListingType} from '../types/listing.type';
+    import { onMount } from 'svelte';
+    export let listingsData: ListingType[];
+    export let mapId: string;
+    console.log("listings Data ", listingsData)
+    let map: google.maps.Map;
+    const center: google.maps.LatLngLiteral = {lat: 30, lng: -110};
+
+    onMount(async () => {
+        console.log("generating map...")
+        map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
+            center,
+            zoom: 10,
+            mapId
+        });
+        const marker = new google.maps.Marker({
+            position: center,
+            map: map,
+        });
+        console.log("map generated...", map)
+    })
+</script>
+
+
+<!-- 
+    The `defer` attribute causes the callback to execute after the full HTML
+    document has been parsed. For non-blocking uses, avoiding race conditions,
+    and consistent behavior across browsers, consider loading using Promises
+    with https://www.npmjs.com/package/@googlemaps/js-api-loader.
+-->
+
+<h3>My Google Maps Demo</h3>
+<!--The div element for the map -->
+<div id="map" style="height: 60vh;"></div>
+
+
+
+
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,17 +1,31 @@
 <script lang="ts">
     import AllListings from '../components/AllListings.svelte';
+    import GoogleMap from '../components/GoogleMap.svelte';
     import type {ListingType} from '../types/listing.type';
-    import {onMount} from "svelte";
     /** @type {import('./$types').PageData} */
     export let data: any;
+    const mapId = "1ec9baf35d56fe9b";
+    const API_KEY = "AIzaSyCqi0nYziVYz6uzgWO5d8Q-Mlb0F5NzhrA";
     let pagination = 0;
     let pageLimit = 50;
+    let ready = true;
+    // Initialize and add the map
 </script>
 
 <style>
 </style>
 
+<svelte:head>
+    <script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCqi0nYziVYz6uzgWO5d8Q-Mlb0F5NzhrA&callback"defer async></script>
+</svelte:head>
+
+{#if ready}
+<GoogleMap listingsData={data.content} mapId={mapId}></GoogleMap>
+{/if}
 <!--HTML BELOW-->
 <AllListings listingsData={data.content}/>
+
+
 
 
