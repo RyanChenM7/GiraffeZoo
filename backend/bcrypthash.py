dfae3fe883084bf495f37e501d98d0fc919e8049
Ryan Chen <54915967+RyanChenM7@users.noreply.github.com>
2022-11-30 21:38:39 -0500

    Update bcrypthash.py

--- backend/bcrypthash.py
+++ backend/bcrypthash.py
@@ -1,9 +1,18 @@
 import bcrypt
 
+ROUNDS = 10
+
 def hash(password: str) -> str:
-    salt = bcrypt.gensalt()
-    hashed = bcrypt.hashpw(password, salt)
+    bytes = password.encode('utf-8')
+    salt = bcrypt.gensalt(rounds=ROUNDS)
+    hashed = bcrypt.hashpw(bytes, salt)
     return hashed
 
 def auth(pwd: str, hash: str) -> bool:
     return bcrypt.checkpw(pwd, hash)
+    
+
+print(hash("password0"))
+print(hash("password1"))
+print(hash("password2"))
+print(hash("password3"))
